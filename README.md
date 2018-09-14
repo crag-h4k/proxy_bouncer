52a6e9473343db5f7f18f2dd774a15c12da444f2
deadlift1226 <danemorgan91@gmail.com>
2018-09-14 15:17:36 -0600

    fixed it

--- README.md
+++ README.md
@@ -1,12 +1,41 @@
 # Bouncer
 
-[ https://free-proxy-lost.net ]
+[ https://free-proxy-list.net ]
 
 To save a log of anonymous active proxies use the command
 
-` python3 bouncer.py `
+`get_proxy(log=True)`
 
-Used as a function in another program, get_proxy() will return a random it will a dictionary of a proxy as shown in the example below:
+Used as a function in another program, get_proxy() will return a random it will a proxy oject as shown in the example below:
 
-` ['ip_addr':'103.18.133.34', 'port':'21776', 'code':'ID', 'country':'Indonesia', 'anon_level':'elite proxy', 'google':'no', 'https':'no'] `
+` P = get_proxy()
+
+print(P.ip_addr)
+        
+        '103.18.133.34'
+
+print(P.port)
+
+        '21776'
+
+print(P.code)
+        
+        'ID'
+
+print(P.country)
+    
+        'Indonesia'
+
+print(P.anon_level)
+        
+        'elite proxy'
+
+print(P.google)
+        
+        'no'
+
+print(P.https
+
+        'no'
+`
 
--- bouncer.py
+++ bouncer.py
@@ -6,8 +6,18 @@ from datetime import datetime
 
 from requests import get
 from bs4 import BeautifulSoup
+class proxy:
+    def __init__(self, ip_addr, port, code, country, anon_level, google, https, last_checked):
+        self.ip_addr = ip_addr
+        self.port = port
+        self.code = code
+        self.country = country
+        self.anon_level = anon_level
+        self.google = google
+        self.https = https
+        self.last_checked = last_checked
 
-def get_proxy():
+def get_proxy(log=False):
     html = get('https://free-proxy-list.net/anonymous-proxy.html').text
     soup = BeautifulSoup(html, 'html.parser')
     proxy_arr = []
@@ -26,17 +36,16 @@ def get_proxy():
             continue
         else:
             proxy_arr.append(td_data)
-            file_data = str(datetime.now()).split('.',1)[0]+'\t'+str(td_data)+'\n'
-            with open(fname,'a+') as f:
-                f.write(file_data)
+            if log == True:
+                file_data = str(datetime.now()).split('.',1)[0]+'\t'+str(td_data)+'\n'
+                with open(fname,'a+') as f:
+                    f.write(file_data)
 
     proxy_arr.pop(0)
-    proxy = choice(proxy_arr)
-    proxy = {'ip_addr':proxy[0], 'port':proxy[1], 'code':proxy[2],
-              'country':proxy[3], 'anon_level':proxy[4], 'google':proxy[5],
-              'https':proxy[6], 'last_checked':proxy[7]}
+    p = choice(proxy_arr)
+    P = proxy(ip_addr = p[0], port = p[1], code = p[2], country = p[3],anon_level = p[4], google = p[5], https = p[6], last_checked = p[7])
 
-    return proxy
+    return P
 
-proxy = get_proxy()
-print(proxy)
+#proxy = get_proxy()
+#print(proxy)
